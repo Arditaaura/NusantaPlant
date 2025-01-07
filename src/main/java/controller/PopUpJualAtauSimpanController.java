14239924609e8d1a11a72ed8c814d0bf86cef0ef
Arditaaura <148346080+Arditaaura@users.noreply.github.com>
2025-01-08 06:10:22 +0700

    Update PopUpJualAtauSimpanController.java

--- src/main/java/controller/PopUpJualAtauSimpanController.java
+++ src/main/java/controller/PopUpJualAtauSimpanController.java
@@ -4,6 +4,9 @@
  */
 package controller;
 
+import static controller.LoginController.user;
+import dao.StatusDAO;
+import dao.UserDAO;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,9 +16,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import java.util.UUID;
 
 /**
  * FXML Controller class
@@ -41,12 +46,23 @@ public class PopUpJualAtauSimpanController implements Initializable {
     private void simpanTanaman(ActionEvent event) {
         try {
             InventoryController.setTanaman(GamePlayController.tanaman);
-            GamePlayController.tanaman = null;
-            Stage stage = (Stage) btnSimpan.getScene().getWindow();
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/Inventory.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
-            stage.setTitle("Inventory");
-            stage.setScene(scene);
+            UserDAO.updateTanamanId(LoginController.user.getUid(), 0);
+            GamePlayController.tanaman= null;
+            Stage popupStage = (Stage) btnSimpan.getScene().getWindow();
+
+            // Mendapatkan stage utama
+            Stage mainStage = (Stage) popupStage.getOwner();
+
+            // Memuat FXML baru untuk scene yang ingin ditampilkan
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Inventory.fxml"));
+            Parent newRoot = loader.load();
+
+            // Mengubah scene dari main stage
+            mainStage.setScene(new Scene(newRoot));
+            mainStage.setTitle("Inventory");
+
+            // Menutup popup
+            popupStage.close();
         } catch (IOException ex) {
             Logger.getLogger(PopUpJualAtauSimpanController.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -56,14 +72,28 @@ public class PopUpJualAtauSimpanController implements Initializable {
     @FXML
     private void jualTanaman(ActionEvent event) {
         try {
+            UUID uid = LoginController.user.getUid();
             int hargaJual = GamePlayController.tanaman.getHargaJual();
-            int honey = GamePlayController.getHoney();
+            int honey = LoginController.user.getPoin();
+            StatusDAO.TransaksiJualTanaman (LoginController.user.getUid(), StatusDAO.getTanaman_id(uid), user.getPoin());
             GamePlayController.tanaman= null;
-            Stage stage = (Stage) btnJual.getScene().getWindow();
-            FXMLLoader fxmlLoader = new FXMLLoader(LoginController.class.getResource("/view/Shop.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
-            stage.setTitle("Shop");
-            stage.setScene(scene);
+            UserDAO.updateTanamanId(LoginController.user.getUid(), 0);
+            LoginController.user.setPoin(honey + hargaJual);
+            Stage popupStage = (Stage) btnJual.getScene().getWindow();
+
+            // Mendapatkan stage utama
+            Stage mainStage = (Stage) popupStage.getOwner();
+
+            // Memuat FXML baru untuk scene yang ingin ditampilkan
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Shop.fxml"));
+            Parent newRoot = loader.load();
+
+            // Mengubah scene dari main stage
+            mainStage.setScene(new Scene(newRoot));
+            mainStage.setTitle("Shop");
+
+            // Menutup popup
+            popupStage.close();
         } catch (IOException ex) {
             Logger.getLogger(PopUpJualAtauSimpanController.class.getName()).log(Level.SEVERE, null, ex);
         }
